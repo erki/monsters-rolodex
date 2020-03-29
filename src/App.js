2f41cc42e4a9147116c43c9f8cc55572069986ec
Christoph Erkinger <christoph.erkinger@gmail.com>
2020-03-29 23:33:21 +0200

    Add remaining features for monsters-rolodex app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React, {Component} from 'react';
 import './App.css';
+import {CardListComponent} from "./components/card-list/card-list.component";
+import {SearchBox} from "./components/search-box/search-box.component";
 
 class App extends Component {
-  constructor(state) {
-    super(state);
+    constructor(state) {
+        super(state);
 
-    this.state = {
-      monsters : []
+        this.state = {
+            monsters: [],
+            searchField: ''
+        }
+
     }
-  }
 
-  /**
-   * Wird aufgerufen bevor eine Component in den DOM geladen wird.
-   * Lädt alle Monster mittels fetch und updated den state
-   * @returns {*}
-   */
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({monsters: users}))
-  }
+    updateSearchString = (searchValue) => {
+        this.setState({searchField: searchValue});
+    };
 
-  render() {
-    return (
-        <div className="App">
-          {
-            this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-          }
-        </div>
-    );
-  }
+    /**
+     * Wird aufgerufen bevor eine Component in den DOM geladen wird.
+     * Lädt alle Monster mittels fetch und updated den state
+     * @returns {*}
+     */
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({monsters: users}))
+    }
+
+    render() {
+        const {monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+        return (
+            <div className="App">
+                <h1>Monster Rolodex</h1>
+                <SearchBox placeHolder='Search Monsters'
+                           handleChange={this.updateSearchString}/>
+                <CardListComponent monsters={filteredMonsters}/>
+            </div>
+        );
+    }
 }
 
 export default App;
