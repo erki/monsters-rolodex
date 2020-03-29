06a5f66d6a3942b8c41a8e5ecd881b4d62ea36f6
Christoph Erkinger <christoph.erkinger@gmail.com>
2020-03-29 23:39:41 +0200

    Updates

---
+++ precache-manifest.4f0b63fe7bda248dcd485cf0e083c318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c708c35c81eed60b2205d482b57a125",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7e9b648a9a9d4fd5523b",
+    "url": "/monsters-rolodex/static/css/main.84a0200a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9b648a9a9d4fd5523b",
+    "url": "/monsters-rolodex/static/js/main.052fdf9b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
